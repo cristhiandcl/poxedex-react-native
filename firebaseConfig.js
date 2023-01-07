40165c3a37a68c4dfc05ed377da50de7e5c850e3
Cristhian Carrillo <carrillocristhian2408@gmail.com>
2023-01-06 21:30:59 -0500

    firebase

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -9,14 +9,12 @@ import { initializeApp } from "firebase/app";
 
 // Initialize Firebase
 const firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+  apiKey: "AIzaSyBh2donaty98LG550TmrRpQXJm-XmtvIsc",
+  authDomain: "pokedex-firebase-b99e2.firebaseapp.com",
+  projectId: "pokedex-firebase-b99e2",
+  storageBucket: "pokedex-firebase-b99e2.appspot.com",
+  messagingSenderId: "8939602846",
+  appId: "1:8939602846:web:448081bf3749d2e7824c8f",
 };
 
 const app = initializeApp(firebaseConfig);
