4cf005c3556637030d868a1e33f9f342ae4a4952
Cristhian Carrillo <carrillocristhian2408@gmail.com>
2023-01-06 22:07:08 -0500

    form

--- components/Login.js
+++ components/Login.js
@@ -1,10 +1,42 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
 
 const Login = () => {
+  const [userData, onChangeText] = React.useState({
+    email: null,
+    password: null,
+  });
+
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-3xl font-bold">Login</Text>
+    <View className="flex-1 border-2 items-center justify-center">
+      <View className="w-2/3 space-y-6">
+        <View className="space-y-2">
+          <Text className="font-extrabold">E-mail</Text>
+          <TextInput
+            className="border-2 p-2"
+            value={userData.email}
+            onChangeText={onChangeText}
+            placeholder="Email"
+          />
+        </View>
+        <View className="space-y-2">
+          <Text className="font-extrabold">Password</Text>
+          <TextInput
+            className="border-2 p-2"
+            value={userData.email}
+            onChangeText={onChangeText}
+            placeholder="Password"
+          />
+        </View>
+        <TouchableOpacity className="bg-green-500 p-2 rounded">
+          <Text className="text-center font-bold text-white">Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity className="bg-blue-500 p-2 rounded">
+          <Text className="text-center font-bold text-white">
+            Create Account
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
