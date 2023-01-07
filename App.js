69394e70dd0001a4810414194da334b4943203e6
Cristhian Carrillo <carrillocristhian2408@gmail.com>
2023-01-06 21:45:56 -0500

    firebase

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
-import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import Login from "./components/Login";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-red-600 font-bold">
-        Open up App.js to start working on your app!
-      </Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
---
+++ components/Login.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Login = () => {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-3xl font-bold">Login</Text>
+    </View>
+  );
+};
+
+export default Login;
