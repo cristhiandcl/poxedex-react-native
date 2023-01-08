7cbadcc27ccada95b5a366c3f90415ff1cb93f74
Cristhian Carrillo <carrillocristhian2408@gmail.com>
2023-01-07 21:57:12 -0500

    google

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,27 +1,42 @@
 import { View, Text, TextInput, TouchableOpacity, Image } from "react-native";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   getAuth,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   GoogleAuthProvider,
-  signInWithPopup,
+  signInWithCredential,
 } from "firebase/auth";
 import app from "../firebaseConfig";
 import { useNavigation } from "@react-navigation/native";
-// import google from "../assets/google.png";
+import * as WebBrowser from "expo-web-browser";
+import * as Google from "expo-auth-session/providers/google";
 
 const auth = getAuth(app);
-const provider = new GoogleAuthProvider();
+WebBrowser.maybeCompleteAuthSession();
 
 const Login = () => {
   const [userData, onChangeText] = useState({
     email: "",
     password: "",
   });
+  const [request, response, promptAsync] = Google.useIdTokenAuthRequest({
+    clientId:
+      "8939602846-p68pru6fb4skkbfgs5jv09bbsrpng5qr.apps.googleusercontent.com",
+  });
 
   const [errorMessage, setErrorMessage] = useState("");
   const navigation = useNavigation();
+
+  useEffect(() => {
+    if (response?.type === "success") {
+      const { id_token } = response.params;
+      const auth = getAuth();
+      const credential = GoogleAuthProvider.credential(id_token);
+      console.log(id_token);
+      signInWithCredential(auth, credential);
+    }
+  }, [response]);
 
   const logUser = () => {
     console.log(userData);
@@ -59,29 +74,7 @@ const Login = () => {
       });
   };
 
-  const signInWithGoogle = () => {
-    signInWithPopup(auth, provider)
-      .then((result) => {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        const credential = GoogleAuthProvider.credentialFromResult(result);
-        const token = credential.accessToken;
-        // The signed-in user info.
-        const user = result.user;
-        console.log(user);
-        navigation.navigate("Home");
-        // ...
-      })
-      .catch((error) => {
-        // Handle Errors here.
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // The email of the user's account used.
-        const email = error.customData.email;
-        // The AuthCredential type that was used.
-        const credential = GoogleAuthProvider.credentialFromError(error);
-        // ...
-      });
-  };
+  const signInWithGoogle = () => {};
 
   return (
     <View className="flex-1 border-2 items-center justify-center">
@@ -125,7 +118,10 @@ const Login = () => {
         </TouchableOpacity>
         <TouchableOpacity
           className="bg-green-500 p-2 rounded flex-row items-center justify-center space-x-2"
-          onPress={signInWithGoogle}
+          onPress={() => {
+            promptAsync();
+          }}
+          disabled={!request}
         >
           <Image source={require("../assets/google.png")} className="h-6 w-6" />
           <Text className="text-center font-bold text-white">
