6047facd12062a0d4a1ba5e04d3de0755624d4a7
Cristhian Carrillo <carrillocristhian2408@gmail.com>
2023-01-09 23:04:53 -0500

    updates

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,13 +1,6 @@
 import { DefaultTheme } from "@react-navigation/native";
 import { initializeApp } from "firebase/app";
 
-// Optionally import the services that you want to use
-// import {...} from "firebase/auth";
-// import {...} from "firebase/database";
-// import {...} from "firebase/firestore";
-// import {...} from "firebase/functions";
-// import {...} from "firebase/storage";
-
 // Initialize Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyBh2donaty98LG550TmrRpQXJm-XmtvIsc",
