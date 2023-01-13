753191154ad14d32f95434bd88c79ba3a9fee2b1
Cristhian Carrillo <carrillocristhian2408@gmail.com>
2023-01-12 21:58:05 -0500

    pokemonData

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import HomeScreen from "./screens/HomeScreen";
 import LoginScreen from "./screens/LoginScreen";
 import SignInScreen from "./screens/SignInScreen";
 
+const client = axios.create({ baseURL: "https://pokeapi.co/api/v2" });
+
 export default function App() {
   const Stack = createNativeStackNavigator();
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    let tempPokemons = [];
+    async function getPokemons() {
+      for (let i = 1; i <= 20; i++) {
+        const response = await client.get(`pokemon/${i}`);
+        tempPokemons.push(response.data);
+      }
+      setPokemons([...tempPokemons]);
+    }
+    getPokemons();
+  }, []);
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
