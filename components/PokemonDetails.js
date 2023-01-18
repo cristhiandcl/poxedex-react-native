cb29bf2a7780438b2aceb1c6bac88a885e96c489
Cristhian Carrillo <carrillocristhian2408@gmail.com>
2023-01-18 14:35:15 -0500

    pokemonScreen

--- components/PokemonDetails.js
+++ components/PokemonDetails.js
@@ -1,9 +1,18 @@
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, TouchableOpacity } from "react-native";
 import React from "react";
+import { XCircleIcon, XMarkIcon } from "react-native-heroicons/solid";
+import { useNavigation } from "@react-navigation/native";
 
 const PokemonDetails = ({ pokemon }) => {
+  const navigation = useNavigation();
   return (
-    <View className="h-full items-center justify-center">
+    <View className="h-full items-center justify-center relative">
+      <TouchableOpacity
+        className="absolute top-4 right-4"
+        onPress={navigation.goBack}
+      >
+        <XCircleIcon size={50} color="green" />
+      </TouchableOpacity>
       <Image
         source={{
           uri: pokemon.sprites.other["official-artwork"].front_default,
