6ed99f42da4021ed060bd37038f7b8779b8cf560
Cristhian Carrillo <carrillocristhian2408@gmail.com>
2023-01-19 00:54:28 -0500

    update

--- components/PokemonDetails.js
+++ components/PokemonDetails.js
@@ -14,7 +14,7 @@ const PokemonDetails = () => {
   ));
 
   return (
-    <View className="bg-green-700 rounded-3xl flex-row flex-wrap items-baseline space-x-5 justify-around mx-8 space-y-4 p-6">
+    <View className="bg-green-700 rounded-3xl flex-row flex-wrap items-baseline gap-x-4 justify-around mx-8 space-y-4 p-6">
       <View className="">
         <Text className="font-extrabold text-xl text-white">Height</Text>
         <Text className="font-extrabold text-lg">
@@ -26,7 +26,7 @@ const PokemonDetails = () => {
         <Text className="font-extrabold text-lg">Pokemon</Text>
       </View>
       <View>
-        <Text className="font-extrabold text-xl text-white">Weight</Text>
+        <Text className="font-extrabold text-xl text-white ">Weight</Text>
         <Text className="font-extrabold text-lg">
           {(pokemon.weight * 0.1).toFixed(2)} Kg
         </Text>
